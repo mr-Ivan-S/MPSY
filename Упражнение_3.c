9e7919851a461fad171101081c4ce88ad66d57e7
mr-Ivan-S <klonleans@gmail.com>
2025-06-18 13:56:33 +0300

    Упражнение 3
    #include <stdio.h>
    int main(void)
    {
    int A = 10;
    char B = 'F';
    float C = 3.14;
    double D = 3.14159;
    
    printf("Int = %i\nChar = %c\nFloat = %f\nDouble = %f",A,B,C,D);
    return 0;
    }

---
+++ Упражнение_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+int A = 10;
+char B = 'F';
+float C = 3.14;
+double D = 3.14159;
+
+printf("Int = %i\nChar = %c\nFloat = %f\nDouble = %f",A,B,C,D);
+return 0;
+}
