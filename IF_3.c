3271362ed900ea42c7ce9e6b611deca11feb83c1
mr-Ivan-S <klonleans@gmail.com>
2025-06-19 17:37:37 +0300

    Условные конструкции 3
    #include <stdio.h>
    #include <locale.h>
    int main(void)
    {
    char *locale = setlocale(LC_ALL, "");
    int A=4;
    if(A>=4 & A<=14) printf("%i В диапазоне (4..14)",A);
    else printf("%i Вне диапазона (4..14)",A);
    return 0;
    }

---
+++ IF_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <locale.h>
+int main(void)
+{
+char *locale = setlocale(LC_ALL, "");
+int A=4;
+if(A>=4 & A<=14) printf("%i В диапазоне (4..14)",A);
+else printf("%i Вне диапазона (4..14)",A);
+return 0;
+}
